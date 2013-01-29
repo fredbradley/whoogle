6c8f3f21f365b3f69ecec84671f833e6a5b92bd3
Fred Bradley <hello@fredbradley.co.uk>
2013-01-29 10:22:59 +0000

    Fixed Date.TimeZone Funciton AGain

--- inc/config/cron_dbbackup.php
+++ inc/config/cron_dbbackup.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../../../db.whoogle.php';
 
-if (function_exists('date_default_timezone_set') { 
+if (function_exists('date_default_timezone_set')) { 
    date_default_timezone_set('Europe/London'); 
 } else { 
    putenv("TZ=Europe/London"); 
